8f3b43ac502760fb7087934a7f88ae420b5db638
fils-ganesh <124351277+fils-ganesh@users.noreply.github.com>
2023-02-06 16:55:37 +0530

    Updated faq.js

--- blocks/faqs/faqs.js
+++ blocks/faqs/faqs.js
@@ -2,9 +2,9 @@ export default function decorate(block) {
   [...block.children].forEach((row, i) => {
     console.log(row);
     const classes = ['question', 'answer'];
-    classes.forEach((c, j) => {
-      const section = row;
-      if (section) section.classList.add(`faqs-${c}`);
+    classes.forEach((class, index) => {
+      const section = row[index];
+      if (section) section.classList.add(`faqs-${class}`);
     });
   });
 }
