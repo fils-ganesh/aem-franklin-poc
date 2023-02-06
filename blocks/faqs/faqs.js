1a8df4a0d3d1ece71bcd220a231a4b37632d5977
fils-ganesh <124351277+fils-ganesh@users.noreply.github.com>
2023-02-06 15:23:33 +0530

    Created Faq folder and added a new JS file.

---
+++ blocks/faqs/faqs.js
@@ -0,0 +1,8 @@
+export default function decorate(block) {
+  [...block.children].forEach((row, i) => {
+    const classes = ['question', 'answer'];
+    classes.forEach((c, j) => {
+      const section = row.children[j];
+      if (section) section.classList.add(`faq-${c}`);
+    });
+  });
