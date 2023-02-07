c8e75edfae012bc1e41318b7238e809e44e9b4b7
fils-ganesh <124351277+fils-ganesh@users.noreply.github.com>
2023-02-07 18:12:42 +0530

    Updated column.js

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -3,8 +3,8 @@ export default function decorate(block) {
   block.classList.add(`columns-${cols.length}-cols`);
   
   [...block.children].forEach((row, i) => {
-    if (row) {
-      row.classList.add(`col-content-${i}`);
+    if (row.children) {
+      row.children.classList.add(`col-content-${i}`);
     }
   });
 
