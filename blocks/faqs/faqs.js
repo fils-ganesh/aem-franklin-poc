72a5bc2abf3567aa01ac22b4b45f1c0e04f07705
fils-ganesh <124351277+fils-ganesh@users.noreply.github.com>
2023-02-09 02:54:24 +0530

    Delete blocks/faqs directory

--- blocks/faqs/faqs.js
+++
@@ -1,8 +0,0 @@
-export default function decorate(block) {
-  [...block.children].forEach((row, i) => {
-    const classes = ['question', 'answer'];
-    if (row) {
-      row.classList.add(`faqs-${classes[i]}`);
-    }
-  });
-}
